5bfbf7f0b2ea3a120f450d02905903a3319dcfb8
ashishkirtwed <=>
2022-05-19 08:03:38 +0530

    changes unidirection to bidirection relationship  - Introduces new field of type Book and uses JsonManagedReference and JsonBackReference

--- src/main/java/com/test/Entity/Author.java
+++ src/main/java/com/test/Entity/Author.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 public class Author {
@@ -17,6 +20,16 @@ public class Author {
 	
 	private String lastName;
 	private String language;
+	
+	@OneToOne(mappedBy="author")
+	@JsonBackReference
+	private Book book;
+	public Book getBook() {
+		return book;
+	}
+	public void setBook(Book book) {
+		this.book = book;
+	}
 	public int getAuthorId() {
 		return authorId;
 	}
